91c8be8a89a131345ce9079220034d6a0034d617
Alize21 <pusikentut@gmail.com>
2025-07-29 21:09:57 +0700

    Feat: Add user management functionality
    introduces user management features such as fetching, adding,
    updating, and deleting user data.

--- employee-management-fe/src/App.jsx
+++ employee-management-fe/src/App.jsx
@@ -1,9 +1,23 @@
+import { addUser, getUser, updateUser, deleteUser } from "./api/user";
+import { useState, useEffect } from 'react';
+
 function App() {
-  return (
-    <div>
-      <h1>Hello, World!</h1>
-      <p>This is a simple React application.</p>
-    </div>
-  );
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    addUser('12345678', 'sangkayu123', 'employee')
+      .then(setUser)
+      .catch((err) => {
+        setUser({ msg: err.message, error: err.status });
+      })
+  }, []);
+  console.log(user);
+    return (
+      <>
+        <h1>Welcome to Employee Management App</h1>
+        <h2>User Data:</h2>
+        {user ? <p>Message : {user.msg} <br/> Error status : {user.error ? user.error.toString() : 'status ok'}</p> : <p>loading...</p>}
+
+      </>
+    )
 }
 export default App
---
+++ employee-management-fe/src/api/user.js
@@ -0,0 +1,134 @@
+const token = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY4N2UzYzdlN2MzZGZkMWU0ZTMwOTQwYiIsInVzZXJuYW1lIjoiQWxpemVzMTIzIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNzUzNzk3Njk0LCJleHAiOjE3NTM4MDEyOTR9.Ww1Zy836ovUa1xlxxOTJgXTop9iqvDRGTmIDPyAOpBk'
+
+const getUser = async () => {
+    try {
+        const res = await fetch('http://localhost:3000', {
+            headers: {
+                "Content-Type": 'application/json',
+                "authorization": token
+            },
+            method: 'GET'
+        })
+        const data = await res.json()
+
+        if (!res.ok) {
+            const error = new Error()
+            error.message = data.msg || `HTTP error! status: ${res.status}`
+            error.status = data.error 
+            throw error
+        }
+
+        return data
+    } catch (err) {
+        throw err
+    }
+}
+
+const addUser = async (username, password, role) => {
+    const payload = {
+        username,
+        password,
+        role
+    }
+
+    // prevent null or undefined values 
+    Object.keys(payload).forEach(key => {
+        if (payload[key] === null || payload[key] === undefined) {
+            delete payload[key]
+        }
+    })
+    
+    try {
+        const res = await fetch('http://localhost:3000', {
+            headers: {
+                "Content-Type": 'application/json',
+                "authorization": token
+            },
+            method: 'POST',
+            body: JSON.stringify(payload)
+        })
+
+        const data = await res.json()
+
+        if (!res.ok) {
+            const error = new Error()
+            error.message = data.msg || `HTTP error! status: ${res.status}`
+            error.status = data.error
+            throw error
+        }
+
+        return data
+    } catch (err) {    
+        throw err
+    }
+}
+
+const updateUser = async (id, username, role) => {
+    const payload = {
+        id,
+        username,
+        role
+    }
+
+    // prevent null or undefined values 
+    Object.keys(payload).forEach(key => {
+        if (payload[key] === null || payload[key] === undefined) {
+            delete payload[key]
+        }
+    })
+
+    try {
+        const res = await fetch(`http://localhost:3000`, {
+            headers: {
+                "Content-Type": 'application/json',
+                "authorization": token
+            },
+            method: 'PATCH',
+            body: JSON.stringify(payload)
+        })
+
+        const data = await res.json()
+
+        if (!res.ok) {
+            const error = new Error()
+            error.message = data.msg || `HTTP error! status: ${res.status}`
+            error.status = data.error
+            throw error
+        }
+
+        return data
+    } catch (err) {
+        throw err
+    }
+}
+
+const deleteUser = async id => {
+
+    id = id ? { id } : {}
+
+    try {
+        const res = await fetch(`http://localhost:3000`, {
+            headers: {
+                "Content-Type": 'application/json',
+                "authorization": token
+            },
+            method: 'DELETE',
+            body: JSON.stringify(id)
+        })
+
+        const data = await res.json(id)
+
+        if (!res.ok) {
+            const error = new Error()
+            error.message = data.msg || `HTTP error! status: ${res.status}`
+            error.status = data.error
+            throw error
+        }
+
+        return data
+    } catch (err) {
+        throw err
+    }
+}
+
+export { getUser, addUser, updateUser, deleteUser };
